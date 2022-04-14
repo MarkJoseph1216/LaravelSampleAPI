942ab9229f71ccb130a88660ce1000919f5e7ba4
Mark Joseph <markcaron16@gmail.com>
2022-04-14 14:29:02 +0800

    Updating Repository.

--- app/Constants/constant.php
+++ app/Constants/constant.php
@@ -6,8 +6,8 @@ class Constant {
    const DOMAIN1 = "shawnnadonham21.com";
    const DOMAIN1_2 = "synthiabenevento22.com";
 
-   const DOMAIN2 = "shawnnadonham21.com";
-   const DOMAIN2_2 = "synthiabenevento22.com";
+   const DOMAIN2 = "cfzhenpin.com";
+   const DOMAIN2_2 = "lihuimold.com";
 }
 
 ?>
